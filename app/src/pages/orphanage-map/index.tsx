a72733339e9727787c3f359521b587eeb87bca04
John Vidal <ba.john.vidal@gmail.com>
2020-10-12 18:11:46 -0300

    basic icon added

--- app/src/pages/orphanage-map/index.tsx
+++ app/src/pages/orphanage-map/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
-import { Map, TileLayer } from "react-leaflet";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 
 import MapMarker from "../../images/map-marker.svg";
 
 import "./styles.css";
+
+const pointerIcon = new Icon({
+  iconUrl: MapMarker,
+  iconRetinaUrl: MapMarker,
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [50, 100],
+  shadowSize: [68, 95],
+  shadowAnchor: [20, 92],
+});
 
 function OrphanageMap() {
   return (
@@ -31,7 +42,7 @@ function OrphanageMap() {
       </Link>
 
       <Map
-        center={[-23.5591229, -46.6483427]}
+        center={[-22.77799, -45.192997]}
         zoom={15}
         style={{ width: "100%", height: "100%" }}
       >
@@ -39,6 +50,7 @@ function OrphanageMap() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+        <Marker position={[-22.77799, -45.192997]} icon={pointerIcon} />
       </Map>
     </div>
   );
