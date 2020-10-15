14b5408d5a545941813c50b5063cc1afb26bf589
John Vidal <ba.john.vidal@gmail.com>
2020-10-15 00:35:29 -0300

    file upload working

--- packages/backend/src/controllers/Orphanages.controller.ts
+++ packages/backend/src/controllers/Orphanages.controller.ts
@@ -1,25 +1,50 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 
-import Orphanages from "../models/Orphanages";
+import Orphanage from "../models/Orphanage";
 
 export default {
   async index(req: Request, res: Response) {
-    const orphanagesRepository = getRepository(Orphanages);
+    const orphanagesRepository = getRepository(Orphanage);
 
     const orphanages = await orphanagesRepository.find();
 
     return res.json(orphanages);
   },
 
+  async show(req: Request, res: Response) {
+    let id = req.params.id;
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return res.json(orphanage);
+  },
+
   async create(req: Request, res: Response) {
     const { name, latitude, longitude, instructions, about, opening_hours, open_on_weekends } = req.body;
 
-    const orphanagesRepository = getRepository(Orphanages);
+    const orphanageRepository = getRepository(Orphanage);
 
-    const orphanage = orphanagesRepository.create({ name, latitude, longitude, instructions, about, opening_hours, open_on_weekends });
+    const requestImage = req.files as Express.Multer.File[];
+    const images = requestImage.map((image) => {
+      return {
+        path: image.filename,
+      };
+    });
 
-    await orphanagesRepository.save(orphanage);
+    const orphanage = orphanageRepository.create({
+      name,
+      latitude,
+      longitude,
+      instructions,
+      about,
+      opening_hours,
+      open_on_weekends,
+      images,
+    });
+
+    await orphanageRepository.save(orphanage);
 
     res.status(201).json(orphanage);
   },
--- packages/backend/src/routes.ts
+++ packages/backend/src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
+
+import uploadConfig from "./config/upload";
 
 import OrphanagesController from "./controllers/Orphanages.controller";
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get("/orphanages", OrphanagesController.index);
-routes.post("/orphanage/create", OrphanagesController.create);
+routes.get("/orphanages/:id", OrphanagesController.show);
+routes.post("/orphanage", upload.array("images"), OrphanagesController.create);
 
 export default routes;
